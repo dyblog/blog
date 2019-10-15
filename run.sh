699cdd1ab62dad9c5a3daf81b0e4ed23d9c60c4a
raoshihong <jsraoshihong@163.com>
2019-10-15 23:44:00 +0800

    编写文章

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-lsof -i:8080 | awk '{if(NR>1){print $2}}' | uniq | awk '{cmd = "kill -9 "$0;system(cmd)}'
+lsof -i:80 | awk '{if(NR>1){print $2}}' | uniq | awk '{cmd = "kill -9 "$0;system(cmd)}'
 bundle exec jekyll serve -w --host=0.0.0.0 > blog.log 2>&1 &
 
